e27890e03d9ce57e62522ab0c36c85b562b45175
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-10-19 19:42:48 -0600

    Add org_id, project_id and raise OpenAIError if missing

--- api/openai_client.py
+++ api/openai_client.py
@@ -1,15 +1,29 @@
 from decouple import config
 from openai import OpenAI
+from openai._exceptions import OpenAIError
 from openai.types.chat.chat_completion import ChatCompletion
 
 
 class OpenAIAPI:
     def __init__(self):
-        api_key = config("OPENAI_API_KEY")
-        self.client = OpenAI(api_key=api_key)
-        self.model = config("OPENAI_MODEL")
-        self.max_tokens = config("MAX_TOKENS", cast=int, default=4096)
-        self.temperature = config("TEMPERATURE", cast=float, default=0.7)
+        api_key: str = config("OPENAI_API_KEY")
+        organization: str = config("OPENAI_ORG_ID")
+        project: str = config("OPENAI_PROJECT_ID")
+        self.model: str = config("OPENAI_MODEL")
+        self.max_tokens: int = config("MAX_TOKENS", cast=int, default=4096)
+        self.temperature: float = config(
+            "TEMPERATURE", cast=float, default=0.7
+        )
+
+        if not api_key:
+            raise OpenAIError("OPENAI_API_KEY not set")
+        if not organization:
+            raise OpenAIError("OPENAI_ORG_ID not set")
+        if not project:
+            raise OpenAIError("OPENAI_PROJECT_ID not set")
+        self.client = OpenAI(
+            api_key=api_key, organization=organization, project=project
+        )
 
     def call_api(self, messages: list[dict]) -> ChatCompletion:
         return self.client.chat.completions.create(
