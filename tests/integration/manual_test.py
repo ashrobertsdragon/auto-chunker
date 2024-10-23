2c044dccc8f8e248f5af08233dc9e25c05b17fab
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-10-23 12:58:03 -0600

    Add  integration test and sort tests into integration and unit

---
+++ tests/integration/manual_test.py
@@ -0,0 +1,39 @@
+import csv
+from pathlib import Path
+
+import pytest
+
+from api.chunking import chunk_text
+from api.chunking_method import ChunkingMethod
+from api.write_csv import create_csv_str
+
+
+@pytest.fixture
+def book() -> str:
+    with Path("tests", "integration", "sample", "test_sample.txt").open(
+        "r"
+    ) as f:
+        return f.read()
+
+
+def test_all(book):
+    csv_contents: dict[str, str] = {}
+    role = "system"
+    chapters = len(book.split("***"))
+    for method in ChunkingMethod:
+        chunks, user_messages = chunk_text(book, method)
+        csv_contents[method.value] = create_csv_str(
+            chunks, user_messages, role
+        )
+
+    assert len(csv_contents) == len(ChunkingMethod)
+    for method_name, csv_str in csv_contents.items():
+        reader = csv.reader(csv_str.splitlines())
+        rows = list(reader)
+        for i, row in enumerate(rows, start=1):
+            assert (
+                len(row) == 3
+            ), f"Row {i + 1} in CSV for {method_name} has {len(row)} columns, expected 3"
+        assert (
+            len(rows) == chapters
+        ), f"CSV for {method_name} has {len(rows)} rows, expected {chapters}"
