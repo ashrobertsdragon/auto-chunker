75eab707cdb8213e936529a4c0b8e9372930f7d3
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-10-26 13:26:13 -0600

    Rename server

--- api/server.py
+++ api/server.py
@@ -1,19 +1,43 @@
 import grpc
 from concurrent import futures
+from decouple import config
+
 import api._proto.auto_chunker_pb2 as auto_chunker
 import api._proto.auto_chunker_pb2_grpc as auto_chunker_grpc
 import api._proto.jsonl_file_creator_pb2 as jsonl_file_creator
 import api._proto.jsonl_file_creator_pb2_grpc as jsonl_file_creator_grpc
-
 import api.write_csv as write_csv
 from api.chunking import chunk_text
 
 
-def chunk(request: auto_chunker.ChunkRequest) -> auto_chunker.ChunkResponse:
+class Authentication:
+    def __init__(self):
+        self.service_token: str = config("SERVICE_TOKEN")
+        self.authentication_status: bool = False
+
+    def authenticate(self, context) -> bool:
+        metadata = dict(context.invocation_metadata())
+        auth_token = metadata.get("authorization", "").replace("Bearer ", "")
+        if auth_token == self.service_token:
+            self.authentication_status = True
+        else:
+            context.set_code(grpc.StatusCode.UNAUTHENTICATED)
+            context.set_details("Invalid authentication credentials")
+        return self.authentication_status
+
+
+def chunk(
+    request: auto_chunker.ChunkRequest, context: grpc.ServicerContext
+) -> auto_chunker.ChunkResponse:
+    auth = Authentication().authenticate(context)
+    if not auth:
+        return auto_chunker.ChunkResponse(
+            jsonl_content="", status_message=context.details()
+        )
     work: tuple[list[str], list[str]] | auto_chunker.ChunkResponse = (
         chunk_text(request.text_content, request.chunking_method)
     )
-    if work.status_message:
+    if hasattr(work, "status_message"):
         return work
     chunks, user_messages = work
     csv_content = write_csv.create_csv_str(chunks, user_messages, request.role)
@@ -28,15 +52,16 @@ def chunk(request: auto_chunker.ChunkRequest) -> auto_chunker.ChunkResponse:
     )
 
 
-def serve() -> None:
+def get_server() -> None:
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     auto_chunker_grpc.add_AutoChunkerServicer_to_server(
         auto_chunker_grpc.AutoChunkerServicer(Chunk=chunk), server
     )
-    server.add_listen_addr("[::]:50052", grpc.insecure_port)
-    server.start()
-    server.wait_for_termination()
+    server.add_secure_port("[::]:50052", grpc.local_server_credentials())
+    return server
 
 
 if __name__ == "__main__":
-    serve()
+    server = get_server()
+    server.start()
+    server.wait_for_termination()
