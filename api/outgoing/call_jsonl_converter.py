9e14facd7eb4038f479e14394c37e9411e987ac5
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-11-02 16:08:44 -0600

    Add API key header

--- api/outgoing/call_jsonl_converter.py
+++ api/outgoing/call_jsonl_converter.py
@@ -2,8 +2,28 @@ import requests
 from decouple import config
 from requests.exceptions import RequestException
 
-from api.errors._exceptions import BadRequestError
+from api.errors._exceptions import APIError, BadRequestError
 from api.errors.error_handling import error_handle
+
+
+def get_jsonl_api_config() -> tuple[str, str]:
+    """
+    Get the API key and URL from environment variables.
+
+    Returns:
+        tuple[str, str]: A tuple containing the API key and URL.
+
+    Raises:
+        APIError: If the API key or URL is not found in the environment
+            variables.
+    """
+    api_key: str = config("JSONL_API_KEY", default="", cast=str)
+    api_url: str = config("JSONL_API_URL", default="", cast=str)
+    if not api_key:
+        raise APIError("JSONL_API_KEY not found in environment variables")
+    if not api_url:
+        raise APIError("JSONL_API_URL not found in environment variables")
+    return api_key, api_url
 
 
 def get_jsonl(csv_str: str, retry_count: int = 0) -> str:
@@ -16,17 +36,20 @@ def get_jsonl(csv_str: str, retry_count: int = 0) -> str:
 
     Returns:
         str: The JSONL content from the API.
-        str: The word 'Error' if there is an error.
     """
+    jsonl_api_key, jsonl_api_url = get_jsonl_api_config()
     try:
         response = requests.get(
-            f"{config('API_URL')}/api/create_jsonl",
+            url=jsonl_api_url,
             params={"csv_str": csv_str},
+            headers={"X-API-KEY": jsonl_api_key},
         )
         if response.status_code == 200:
             return response.content
         else:
             raise BadRequestError(response.status_code)
     except (RequestException, BadRequestError) as e:
-        retry_count = error_handle(e, retry_count)
+        retry_count = error_handle(
+            e, retry_count
+        )  # raises APIError after 5 retries
         return get_jsonl(csv_str, retry_count + 1)
