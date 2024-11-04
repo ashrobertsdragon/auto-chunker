732a88ba08f2a506f1cea2ab1183ee800abbf8e1
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-11-03 22:10:26 -0700

    Use aiohttp instead of requests

--- api/outgoing/call_jsonl_converter.py
+++ api/outgoing/call_jsonl_converter.py
@@ -1,6 +1,6 @@
-import requests
+import aiohttp
 from decouple import config
-from requests.exceptions import RequestException
+from aiohttp.client_exceptions import ClientResponseError
 
 from errors._exceptions import APIError, BadRequestError
 from errors.error_handling import error_handle
@@ -17,16 +17,26 @@ def get_jsonl_api_config() -> tuple[str, str]:
         APIError: If the API key or URL is not found in the environment
             variables.
     """
-    api_key: str = config("JSONL_API_KEY", default="", cast=str)
-    api_url: str = config("JSONL_API_URL", default="", cast=str)
+    api_key: str = config("JSONL_API_KEY", default="")
+    api_url: str = config("JSONL_API_URL", default="")
+    api_port: str = config("JSONL_API_PORT", default="")
+    api_endpoint: str = config("JSONL_API_ENDPOINT", default="")
     if not api_key:
         raise APIError("JSONL_API_KEY not found in environment variables")
     if not api_url:
         raise APIError("JSONL_API_URL not found in environment variables")
-    return api_key, api_url
+    if not api_port:
+        raise APIError("JSONL_API_PORT not found in environment variables")
+    if not api_endpoint:
+        raise APIError("JSONL_API_ENDPOINT not found in environment variables")
+    if not api_url.startswith(("http://", "https://")):
+        api_url = f"http://{api_url}"
+    endpoint_url = f"{api_url}:{api_port}{api_endpoint}"
+
+    return api_key, endpoint_url
 
 
-def get_jsonl(csv_str: str, retry_count: int = 0) -> str:
+async def get_jsonl(csv_str: str, retry_count: int = 0) -> str:
     """
     Get the JSONL content from the
 
@@ -38,18 +48,18 @@ def get_jsonl(csv_str: str, retry_count: int = 0) -> str:
         str: The JSONL content from the
     """
     jsonl_api_key, jsonl_api_url = get_jsonl_api_config()
+    headers = {"X-API-KEY": jsonl_api_key, "Content-Type": "text/csv"}
     try:
-        response = requests.get(
-            url=jsonl_api_url,
-            params={"csv_str": csv_str},
-            headers={"X-API-KEY": jsonl_api_key},
-        )
-        if response.status_code == 200:
-            return response.content
-        else:
-            raise BadRequestError(response.status_code)
-    except (RequestException, BadRequestError) as e:
+        async with aiohttp.ClientSession() as session:
+            async with session.post(
+                url=jsonl_api_url, data=csv_str, headers=headers
+            ) as response:
+                if response.status == 200:
+                    return await response.read()
+                else:
+                    raise BadRequestError(response.status)
+    except (ClientResponseError, BadRequestError) as e:
         retry_count = error_handle(
             e, retry_count
-        )  # raises APIError after 5 retries
+        )  # raises APIError after 2 retries
         return get_jsonl(csv_str, retry_count + 1)
